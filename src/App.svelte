46fd32b751158bdeb76cff9d5aa995d4aa098228
thejavierCO <javier.cruz.odispo@hotmail.com>
2023-03-15 16:30:53 -0600

    testing smui

--- src/App.svelte
+++ src/App.svelte
@@ -1,17 +1,27 @@
 <script>
   import Button from "@smui/button";
+  import TopAppBar, { Row, Section, Title } from "@smui/top-app-bar";
   import Card, { Content, Actions } from "@smui/card";
 </script>
 
 <main>
-  <Card>
-    <Content>
-      <h3>Hello</h3>
-    </Content>
-    <Actions fullBleed>
-      <Button>Work</Button>
-    </Actions>
-  </Card>
+  <TopAppBar variant="static" color="secondary">
+    <Row>
+      <Section>
+        <Title>Calculators</Title>
+      </Section>
+    </Row>
+  </TopAppBar>
+  <Content>
+    <Card>
+      <Content>
+        <h3>Hello</h3>
+      </Content>
+      <Actions fullBleed>
+        <Button>Work</Button>
+      </Actions>
+    </Card>
+  </Content>
 </main>
 
 <style>
