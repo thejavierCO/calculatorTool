7de2af21d308b836f1f4d40296a7afd1400181a9
thejavierCO <javier.cruz.odispo@hotmail.com>
2023-04-02 18:39:40 -0500

    update component app

--- src/App.svelte
+++ src/App.svelte
@@ -1,19 +1,9 @@
 <script>
   import Title from "./Components/Title.svelte";
   import Content from "./Components/Content.svelte";
-  import Form from "./Components/Form.svelte";
-  let show = true;
-  function add({ detail: a }) {
-    console.log(a);
-    show = !show;
-  }
 </script>
 
 <main>
   <Title value="Working" />
-  <Content>
-    {#if show}
-      <Form on:save={add} />
-    {/if}
-  </Content>
+  <Content />
 </main>
