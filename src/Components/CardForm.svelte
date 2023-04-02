0000e1fb2c5441ebc42d4303a694ba28afa1ef41
thejavierCO <javier.cruz.odispo@hotmail.com>
2023-04-02 18:40:09 -0500

    add new component for testing

---
+++ src/Components/CardForm.svelte
@@ -0,0 +1,22 @@
+<script>
+  import Card, { Content, Actions } from "@smui/card";
+  import LayoutGrid, { Cell } from "@smui/layout-grid";
+  import Button, { Label } from "@smui/button";
+</script>
+
+<Card>
+  <Content>
+    <LayoutGrid>
+      <Cell span={12}>
+        <Button>
+          <Label style="text-align:center">Save</Label>
+        </Button>
+      </Cell>
+    </LayoutGrid>
+    <Actions fullBleed>
+      <Button>
+        <Label style="text-align:center">Save</Label>
+      </Button>
+    </Actions>
+  </Content>
+</Card>
