8cbc66a9f07a44b9ab8c217e9f34c67c31976b5f
thejavierCO <javier.cruz.odispo@hotmail.com>
2023-04-03 14:33:03 -0500

    add tools

---
+++ src/js/tools.js
@@ -0,0 +1,59 @@
+class Events extends EventTarget {
+  emit(type, data) {
+    this.dispatchEvent(!data ? new Event(type) : new CustomEvent(type, { detail: data }))
+  }
+  on(type, fns) {
+    this.addEventListener(type, fns)
+  }
+}
+
+export class Store extends Events {
+  constructor(id) {
+    super();
+    if (!id) throw "Require id";
+    this.id = id;
+  }
+  get data() {
+    return localStorage.getItem(this.id)
+  }
+  set data(value) {
+    if (typeof value === "string") localStorage.setItem(this.id, value)
+    else throw "require string data"
+  }
+}
+
+export class Temporisador extends Events {
+  constructor(time = 0) {
+    super();
+    this._timeCounter = time;
+    this._time = 0;
+    this._interval = 1000 / 10;
+    this.Temp = undefined;
+  }
+
+  start() {
+    this.Temp = setInterval(() => {
+      if (this._time < this._timeCounter) {
+        this._time = this._time + this._timeCounter / this._interval;
+        this.emit("start", { time: this.time, total: this._timeCounter });
+        this.on("pause", () => clearInterval(this.Temp))
+      } else if (this._time > this._timeCounter) {
+        this.emit("stop");
+      }
+    }, this._interval)
+    this.on("stop", () => {
+      this._time = 0;
+      clearInterval(this.Temp);
+    })
+    return { start: (fns) => this.on("start", fns), stop: (fns) => this.on("stop", fns) };
+  }
+  pause() {
+    this.emit("pause")
+  }
+  clear() {
+    this.emit("stop")
+  }
+  get time() {
+    return this._time;
+  }
+}
