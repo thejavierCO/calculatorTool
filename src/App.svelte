49158b6a77698ef7a757fee73eb21ecc108c78f3
thejavierCO <javier.cruz.odispo@hotmail.com>
2023-04-03 19:36:20 -0500

    clear

--- src/App.svelte
+++ src/App.svelte
@@ -2,21 +2,11 @@
   import Title from "./Components/Title.svelte";
   import Card from "./Components/CardForm.svelte";
   import LayoutGrid from "@smui/layout-grid";
+
   import { Cards } from "./js/data";
-  import Timer from "./Components/timer2.svelte";
-  let addItem = ({ detail }) =>
-    Cards.update((a) => {
-      a.push(detail);
-      return a;
-    });
-  let delItem = ({ detail }) => {
-    Cards.update((a) => a.filter((item) => detail.id !== item.id));
-  };
-  let editItem = ({ detail }) => {
-    // Cards.update((e) => {
-    //   return e;
-    // });
-  };
+  let addItem = ({ detail }) => {};
+  let delItem = ({ detail }) => {};
+  let editItem = ({ detail }) => {};
 </script>
 
 <main>
@@ -32,14 +22,7 @@
           on:add={addItem}
           on:del={delItem}
           on:edit={editItem}
-        >
-          <Timer
-            type="temporizer"
-            Time={item.time}
-            progress={item.progress}
-            start={item.start}
-          />
-        </Card>
+        />
       {/each}
     {/if}
   </LayoutGrid>
