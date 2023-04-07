3c33b7afdfd0c09656214f5ed704ff0f8dae60ad
thejavierCO <javier.cruz.odispo@hotmail.com>
2023-04-07 15:38:53 -0500

    add component form

---
+++ src/Components/form.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import Select, { Option } from "@smui/select";
+  import Textfield from "@smui/textfield";
+  import Switch from "@smui/switch";
+  import FormField from "@smui/form-field";
+  import Checkbox from "@smui/checkbox";
+  import Chip, { Set, Text } from "@smui/chips";
+  import Button, { Label } from "@smui/button";
+  import Radio from "@smui/radio";
+</script>
