32539ba133b9d5d950387f45a971e9590a111fde
thejavierCO <javier.cruz.odispo@hotmail.com>
2023-04-07 20:29:26 -0500

    alternativa de temporizador

--- src/App.svelte
+++ src/App.svelte
@@ -2,10 +2,12 @@
   import Title from "./Components/Title.svelte";
   import Card from "./Components/CardForm.svelte";
   import Form from "./Components/form.svelte";
-  import LayoutGrid from "@smui/layout-grid";
+  import LayoutGrid, { Cell } from "@smui/layout-grid";
 
   import { store, Types } from "./js/data";
   import Timer from "./Components/timer.svelte";
+  import Test from "./Components/simpleTimer.svelte";
+  import Button, { Label } from "@smui/button";
   let { base } = store;
 
   let addItem = ({ detail }) => {
@@ -20,14 +22,37 @@
       return data;
     });
   };
+  let updatePosicion = (id, data) => {
+    store.edit(id, (item) => {
+      let { posicion, time } = data;
+      item.progress = (posicion * 1) / time;
+      return item;
+    });
+  };
+
+  let updateStatus = (id, data) => {
+    store.edit(id, (item) => {
+      let { status } = data;
+      item.status = status;
+      return item;
+    });
+  };
+  const temp = ({ detail }) => {
+    console.log(detail);
+  };
 </script>
 
 <main>
   <Title value="Working" />
   <LayoutGrid>
+    <!-- <Test seconds={5} on:Status={temp}>
+      <span>
+        {status}
+      </span>
+    </Test> -->
     {#if $base.length == 0}
-      <Card type="start">
-        <Form {Types} on:done={addItem} />
+      <Card type="start" on:add={addItem}>
+        <!-- <Form {Types} on:done={addItem} /> -->
       </Card>
     {:else}
       {#each $base as item}
@@ -38,14 +63,31 @@
           on:del={delItem}
           on:edit={editItem}
         >
-          <Timer
+          <Cell span={12}>
+            <Test
+              seconds={item.time}
+              posicion={Math.round((item.progress * (item.time * 1000)) / 1)}
+              status={item.status}
+              on:Play={(evt) => updatePosicion(item.id, evt.detail)}
+              on:Status={(evt) => updateStatus(item.id, evt.detail)}
+              let:posicion
+              let:status
+              let:start
+            >
+              <p>{posicion}</p>
+              <Button on:click={start}>
+                <Label>{status == "play" ? "pause" : "play"}</Label>
+              </Button>
+            </Test>
+          </Cell>
+          <!-- <Timer
             id={item.id}
             type="temporizer"
             {store}
             bind:status={item.status}
             bind:progress={item.progress}
             bind:time={item.time}
-          />
+          /> -->
         </Card>
       {/each}
     {/if}
---
+++ src/Components/simpleTimer.svelte
@@ -0,0 +1,72 @@
+<script lang="ts">
+  import {
+    createEventDispatcher,
+    onMount,
+    beforeUpdate,
+    onDestroy,
+  } from "svelte";
+  export let seconds: number = 0;
+  export let posicion: number | undefined = undefined;
+  export let status: "play" | "stop" | "pause" = "stop";
+  export let CPS = 10;
+  const time = seconds * 1000;
+  const interval = 1000 / CPS;
+  const emit = createEventDispatcher();
+  let temp;
+  // let progress = posicion ? posicion : 0;
+  let calc = () => {
+    if (status == "play") status = "pause";
+    else {
+      status = "play";
+      emit("Status", { status });
+      if (!temp) {
+        temp = setInterval(() => {
+          if (posicion < time) {
+            if (status == "play") {
+              posicion = posicion + interval;
+              emit("Play", { posicion: posicion, time });
+            } else if (status == "pause") {
+              clearInterval(temp);
+              emit("Status", { status });
+            }
+          } else if (posicion >= time) {
+            clearInterval(temp);
+            posicion = 0;
+            status = "stop";
+            emit("Status", { status });
+          }
+        }, interval);
+      } else console.log("exist", temp);
+    }
+  };
+  // let once = false;
+  // onMount(() => {
+  //   if (status == "play") {
+  //     status = "pause";
+  //     calc();
+  //   }
+  //   if (posicion >= time) posicion = 0;
+  // });
+  beforeUpdate(() => {
+    if (status == "play") {
+      status = "pause";
+      calc();
+    }
+  });
+  onDestroy(() => {
+    status = "stop";
+  });
+</script>
+
+<slot {time} {posicion} {status} start={calc}>
+  <p>Start:{time}</p>
+  <p>Posicion:{posicion}</p>
+  <p>status:{status}</p>
+  {#if status == "play"}
+    <button on:click={calc}>
+      {status == "play" ? "pause" : "play"}
+    </button>
+  {:else}
+    <button on:click={calc}> play </button>
+  {/if}
+</slot>
