8128356e7ceb9e9e6692f036c0e533ab17a2611c
thejavierCO <javier.cruz.odispo@hotmail.com>
2023-04-07 20:24:05 -0500

    moviendo cards

--- src/js/data.js
+++ src/js/data.js
@@ -2,11 +2,10 @@ import { Store } from "./tools.js"
 import { writable, get, derived } from "svelte/store";
 
 export let store = new Store("store");
+export let Cards = store.base;
 
 export let Types = writable([
   "Bote Negro",
   "Bote dorado",
   "Bote de aluminio"
 ]);
-
-export let Cards = store.base;
