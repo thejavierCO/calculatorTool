85c2a5c13f1803c6b542a4dba524b6152a01161d
thejavierCO <javier.cruz.odispo@hotmail.com>
2023-04-08 01:58:02 -0500

    add clear fn and testing remove component

--- src/Components/simpleTimer.svelte
+++ src/Components/simpleTimer.svelte
@@ -1,64 +1,64 @@
 <script lang="ts">
-  import {
-    createEventDispatcher,
-    onMount,
-    beforeUpdate,
-    onDestroy,
-  } from "svelte";
+  import { beforeUpdate, createEventDispatcher, onDestroy } from "svelte";
   export let seconds: number = 0;
   export let posicion: number | undefined = undefined;
   export let status: "play" | "stop" | "pause" = "stop";
-  export let CPS = 10;
+  export let CPS = 6;
   const time = seconds * 1000;
   const interval = 1000 / CPS;
   const emit = createEventDispatcher();
   let temp;
-  // let progress = posicion ? posicion : 0;
+  if (posicion >= time) posicion = 0;
+  let clear = () => {
+    status = "stop";
+    posicion = 0;
+    emit("Status", { status });
+    emit("Play", { posicion: posicion, time });
+  };
   let calc = () => {
-    if (status == "play") status = "pause";
-    else {
+    if (!temp || status == "stop") {
+      if (temp && status == "stop") clearInterval(temp);
       status = "play";
+      temp = setInterval(() => {
+        if (posicion < time) {
+          if (status == "play") posicion = posicion + interval;
+          emit("Play", { posicion: posicion, time });
+          emit("Status", { status });
+        } else if (posicion >= time) {
+          posicion = 0;
+          status = "stop";
+          emit("Play", { posicion: posicion, time });
+          emit("Status", { status });
+          clearInterval(temp);
+        }
+      }, interval);
       emit("Status", { status });
-      if (!temp) {
-        temp = setInterval(() => {
-          if (posicion < time) {
-            if (status == "play") {
-              posicion = posicion + interval;
-              emit("Play", { posicion: posicion, time });
-            } else if (status == "pause") {
-              clearInterval(temp);
-              emit("Status", { status });
-            }
-          } else if (posicion >= time) {
-            clearInterval(temp);
-            posicion = 0;
-            status = "stop";
-            emit("Status", { status });
-          }
-        }, interval);
-      } else console.log("exist", temp);
+    } else {
+      clearInterval(temp);
+      temp = false;
+      if (posicion < time) status = "pause";
+      emit("Status", { status });
     }
   };
-  // let once = false;
-  // onMount(() => {
-  //   if (status == "play") {
-  //     status = "pause";
-  //     calc();
-  //   }
-  //   if (posicion >= time) posicion = 0;
-  // });
-  // beforeUpdate(() => {
-  //   if (status == "play") {
-  //     status = "pause";
-  //     calc();
-  //   }
-  // });
-  // onDestroy(() => {
-  //   status = "stop";
-  // });
+  beforeUpdate(() => {
+    if (!temp && status == "play") calc();
+    else if (temp && status == "play")
+      console.log(status, posicion, time, interval, temp);
+
+    // if (status == "play") {
+    //   if (once == false) {
+    //     status = "stop";
+    //     calc();
+    //     once = true;
+    //   }
+    // } else if (status == "pause") {
+    //   console.log(status);
+    // }
+  });
+  onDestroy(() => clearInterval(temp));
 </script>
 
-<slot {time} {posicion} {status} start={calc}>
+<slot {time} {posicion} {status} start={calc} {clear}>
   <p>Start:{time}</p>
   <p>Posicion:{posicion}</p>
   <p>status:{status}</p>
