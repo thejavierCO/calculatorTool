dc61a8d8da168da56076525e70c6e8dde0517551
thejavierCO <javier.cruz.odispo@hotmail.com>
2023-04-17 21:28:30 -0500

    new timer

---
+++ src/Components/timer.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import { beforeUpdate, createEventDispatcher, onDestroy } from "svelte";
+
+  export let data: { state: string; time: number; size: number; type: string };
+  let CalcPeerSecond = 10;
+
+  if (data.time > 30) CalcPeerSecond = 3;
+  else if (data.time > 60) CalcPeerSecond = 2;
+  else if (data.time > 120) CalcPeerSecond = 1;
+</script>
