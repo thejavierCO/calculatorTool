e764b9db228e9d6674b96311c08347c00e58cc5c
portalcruz117 <portalcruz117@gmail.com>
2024-08-11 04:08:55 +0000

    Estable 2

--- src/Components/Timer Components/timer.js
+++ src/Components/Timer Components/timer.js
@@ -1,15 +1,15 @@
-import { readable , get} from "svelte/store";
+import { readable, get } from "svelte/store";
 
-export class Clock{
+export class Clock {
   constructor(fns) {
     this._posicion = readable(0, (set) => {
       const updateClock = _ => {
         set(new Date().getTime())
         window.requestAnimationFrame(updateClock);
-        if(fns)fns(this);
+        if (fns) fns(this);
       }
-      let clock =  window.requestAnimationFrame(updateClock);
-      return _ =>window.cancelAnimationFrame(clock);
+      let clock = window.requestAnimationFrame(updateClock);
+      return _ => window.cancelAnimationFrame(clock);
     })
   }
   get subscribe() {
@@ -44,77 +44,77 @@ export class timeFormat {
   }
 }
 
-export default class Timer extends EventTarget{
-  constructor(Miliseconds,status,time) {
+export default class Timer extends EventTarget {
+  constructor(Miliseconds, status, time) {
     super();
-    this.status = status||"Stop";
+    this.status = status || "Stop";
     this.Miliseconds = Miliseconds;
     this.current = 0;
-    this.time = time||{ start: 0, end: 0, pause: 0 };
+    this.time = time || { start: 0, end: 0, pause: 0 };
     this.clock = new Clock();
-    this.Destroy = this.clock.subscribe(t=>{
+    this.Destroy = this.clock.subscribe(t => {
       this.current = t
-      if(this.status == "Play"&&this.current>=this.time.end)this.Stop();
-      this.emit("clock",this.status == "Play"?this.time.end-t:this.status=="Pause"?this.time.end-this.time.pause:0);
-      this.emit("Status");
+      if (this.status == "Play" && this.current >= this.time.end) this.Stop();
+      this.emit("clock", this.status == "Play" ? this.time.end - t : this.status == "Pause" ? this.time.end - this.time.pause : 0);
     });
   }
-  updateTime(data){
+  updateTime(data) {
     this.time = data;
   }
-  updateStatus(data){
+  updateStatus(data) {
     this.status = data;
   }
-  updateMiliseconds(data){
+  updateMiliseconds(data) {
     this.Miliseconds = data;
   }
-  formatTime(data){
+  formatTime(data) {
     return new timeFormat(data);
   }
-  Play(){
-    if(this.time.pause!=0){
+  Play() {
+    if (this.current == 0) throw "currentTime is 0"
+    if (this.time.pause != 0) {
       let posPause = Math.round(this.time.pause - this.time.start),
         timePause = this.time.end - this.time.start,
         timeOff = timePause - posPause;
       this.time.start = this.current;
       this.time.end = this.time.start + timeOff;
       this.time.pause = 0;
-    }else if(this.time.start == 0&&this.time.end == 0){
+    } else if (this.time.start == 0 && this.time.end == 0) {
       this.time.start = this.current;
       this.time.end = this.time.start + this.Miliseconds;
-    }else{
+    } else {
       throw new Error("Timer already running");
     }
     this.status = "Play";
     this.emit("Play");
     this.emit("Status");
   }
-  Pause(){
-    if(this.time.pause==0)this.time.pause = this.current;
+  Pause() {
+    if (this.time.pause == 0) this.time.pause = this.current;
     else throw new Error("Timer already paused");
     this.status = "Pause";
     this.emit("Pause");
     this.emit("Status");
   }
-  Stop(){
-    if(this.time.pause!=0)throw new Error("Timer already paused")
-    if(this.time.start!=0&&this.time.end!=0){
+  Stop() {
+    if (this.time.pause != 0) throw new Error("Timer already paused")
+    if (this.time.start != 0 && this.time.end != 0) {
       this.time.start = 0;
       this.time.end = 0;
       this.time.pause = 0;
-    }else throw new Error("Timer already stopped")
+    } else throw new Error("Timer already stopped")
     this.status = "Stop";
     this.time = { start: 0, end: 0, pause: 0 };
     this.emit("Stop");
     this.emit("Status");
   }
-  on(event,callback){
-    this.addEventListener(event,callback);
-    return ()=>{
-      this.removeEventListener(event,callback);
+  on(event, callback) {
+    this.addEventListener(event, callback);
+    return () => {
+      this.removeEventListener(event, callback);
     }
   }
-  emit(event,data){
-    return this.dispatchEvent(new CustomEvent(event,{detail:data}));
+  emit(event, data) {
+    return this.dispatchEvent(new CustomEvent(event, { detail: data }));
   }
 }
--- src/Components/Timer Components/timer.svelte
+++ src/Components/Timer Components/timer.svelte
@@ -1,6 +1,11 @@
 <script>
-  import { createEventDispatcher, onMount, onDestroy ,beforeUpdate} from "svelte";
-  import Timer from "./timer.js"
+  import {
+    createEventDispatcher,
+    onMount,
+    onDestroy,
+    beforeUpdate,
+  } from "svelte";
+  import Timer from "./timer.js";
   let emit = createEventDispatcher();
 
   export let time = { start: 0, pause: 0, end: 0 };
@@ -8,24 +13,28 @@
   export let seconds = 1;
   export let autoRun = false;
 
-  let timer = new Timer(seconds * 1000,status,time);
+  let timer = new Timer(seconds * 1000, status, time);
   let position = timer.formatTime(0);
 
-  timer.on("clock",({detail:t})=>position = timer.formatTime(t))
-  timer.on("Status", ({target}) => {
+  timer.on("clock", ({ detail: t }) => (position = timer.formatTime(t)));
+  timer.on("Status", ({ target }) => {
     status = target.status;
-    emit("Status", { status,time })
-  })
+    emit("Status", { status, time });
+  });
 
-  onMount(()=>{
-    if(autoRun)timer.Play();
-  })
-  beforeUpdate(()=>{
+  onMount(() => {
+    if (autoRun) {
+      setTimeout(() => timer.Play(), 10);
+    }
+  });
+  beforeUpdate(() => {
     timer.updateTime(time);
     timer.updateStatus(status);
-    timer.updateMiliseconds(seconds*1000);
-  })
-  onDestroy(()=>{timer.Destroy();})
+    timer.updateMiliseconds(seconds * 1000);
+  });
+  onDestroy(() => {
+    timer.Destroy();
+  });
 </script>
 
 <slot
